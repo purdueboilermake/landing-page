e2fe25ee86d5c141de8ea35dfd207f6e6a707ede
Charlie Sale <softwaresale01@gmail.com>
2024-01-19 06:05:19 +0000

    Rework schedule/site to make responsive/day  of ready (#2)

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 /* FAQ CLICK TO EXPAND */
 document.addEventListener("DOMContentLoaded", function () {
     const carouselItems = document.querySelectorAll(".carousel-item");
-
+    
     carouselItems.forEach(function (item) {
         item.addEventListener("click", function () {
             this.classList.toggle("expanded");
-
+            
             // Collapse other expanded items
             carouselItems.forEach(function (otherItem) {
                 if (otherItem !== item) {
@@ -194,85 +194,15 @@ const schedule = {
     ],
 };
 
-/* RENDERING EVENT RECTANGLES */
-function renderEventRectangles(day) {
-    const eventsForDay = schedule[day];
-    const eventRectanglesContainer = document.querySelector('.bottom-left');
-    eventRectanglesContainer.innerHTML = '';
-
-    eventsForDay.forEach(event => {
-        const eventHtml = `
-            <div class="event-rectangle" data-event-id="${event.id}">
-                <div class="event-time">${event.time}</div>
-                <div class="event-title-and-location">
-                    <span>${event.title}</span>
-                    <span>${event.location}</span>
-                </div>
-                <button class="image-button">
-                    <img src="./assets/event-arrow.png" alt="arrow" class="event-arrow" data-event-id="${event.id}">
-                </button>
-            </div>
-        `;
-
-        eventRectanglesContainer.innerHTML += eventHtml;
-    });
-
-    attachEventListeners();
-}
-
-/* ATTACH EVENT LISTENER FOR EVENT-RECTANGLE CLICK */
-function attachEventListeners() {
-    document.querySelectorAll('.event-arrow').forEach(item => {
-        item.addEventListener('click', function() {
-            const eventId = this.getAttribute('data-event-id');
-            displayEventDetails(eventId);
-        });
-    });
-}
-
-
-/* DISPLAY EVENT DETAILS */
-function displayEventDetails(eventId) {
-    const day = document.getElementById('schedule-date').textContent.trim();
-    const event = schedule[day].find(e => e.id === eventId);
-
-    if (event) {
-        let htmlContent = `
-            <span class="event-detail-headers">${event.location}: ${event.title}</span>
-            <span><span class="slackey-font">Presenter: </span>${event.presenter}</span>
-            <span><span class="slackey-font">Link: </span> ${event.companyLink}</span>
-        `;
-
-        document.querySelector('.bottom-right').innerHTML = htmlContent;
+window.customElements.define('bm-calendar-section', class extends HTMLElement {
+    constructor() {
+        super();
     }
-}
-
-/* EVENT LISTENER FOR DATE CHANGE */
-function updateScheduleDate(direction) {
-    const scheduleDates = Object.keys(schedule);
-    let currentDateText = document.querySelector('#schedule-date span').textContent;
-    let currentIndex = scheduleDates.indexOf(currentDateText);
-
-    if (direction === 'next') {
-        currentIndex = (currentIndex + 1) % scheduleDates.length;
-    } else if (direction === 'prev') {
-        currentIndex = (currentIndex - 1 + scheduleDates.length) % scheduleDates.length;
+    
+    connectedCallback() {
+        const template = document.getElementById('calendar-section-container')
+        const content = template.content;
+        const shadowRoot = this.attachShadow({ mode: 'open' });
+        shadowRoot.appendChild(content.cloneNode(true));
     }
-
-    document.querySelector('#schedule-date span').textContent = scheduleDates[currentIndex];
-    renderEventRectangles(scheduleDates[currentIndex]);
-}
-
-/* DEFAULT DATE ON SITE LOAD */
-document.addEventListener('DOMContentLoaded', function() {
-    const initialDate = 'Friday, January 19';
-    renderEventRectangles(initialDate);
-
-    document.querySelector('.arrow-right').addEventListener('click', function() {
-        updateScheduleDate('next');
-    });
-
-    document.querySelector('.arrow-left').addEventListener('click', function() {
-        updateScheduleDate('prev');
-    });
 });

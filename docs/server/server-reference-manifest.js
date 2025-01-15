e0674868b2b56daf572fe4a7302c5a9dc18e23c3
Varun Jasti <varunjasti@pal-nat186-26-170.itap.purdue.edu>
2025-01-14 20:31:50 -0500

    Rollback docs folder to aa0a32a9e4df0c1a19a22636694e17302e5ebdd6 commit

--- docs/server/server-reference-manifest.js
+++ docs/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"fJpH9T28UPPqROPWjgMycohhxtgGv2EKXPTA6JCB0Hk=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"

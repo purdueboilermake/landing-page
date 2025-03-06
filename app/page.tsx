ab6a74b3fdf73b4ad55795f1c12b6f09c42fcdf5
Varun Jasti <varunjasti@Varuns-MacBook-Air.local>
2025-03-06 15:06:35 -0500

    Put organizer application on website

--- app/page.tsx
+++ app/page.tsx
@@ -300,7 +300,7 @@ function App() {
             <div className="text-right">
               <div className="container mx-auto">
                 <div className='sm:pt-[12rem] md:pt-[12rem] lg:pt-[22rem] xl:pt-[22rem]'>
-                  <ApplyButton text="Apply Now!" link='https://boilermake-apply.web.app' size={screenSize === 'sm' ? 'medium' : screenSize === 'md' ? 'medium' : screenSize === 'lg' ? 'large' : screenSize === 'xl' ? 'large' : 'large'} />
+                  <ApplyButton text="Organizer Application" link='https://forms.gle/inJw2FP3UwLMtNcZA' size={screenSize === 'sm' ? 'medium' : screenSize === 'md' ? 'medium' : screenSize === 'lg' ? 'large' : screenSize === 'xl' ? 'large' : 'large'} />
 
                   <ApplyButton text="Devpost" link='https://boilermake-xii.devpost.com/' size={screenSize === 'sm' ? 'medium' : screenSize === 'md' ? 'medium' : screenSize === 'lg' ? 'large' : screenSize === 'xl' ? 'large' : 'large'} />
                 </div>
